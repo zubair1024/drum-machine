7e38e9ece20d7ec664188d20a0524fb457138d61
Zubair Ahmed <zubair1024@gmail.com>
2021-08-12 21:25:01 +0530

    clean up

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import "./Controls.css";
 import Display from "./Display";
@@ -10,41 +10,35 @@ export default function Controls(props) {
 
   const { power, setPower, bank, setBank, volume, setVolume } = props;
 
-  useEffect(() => {
-    // console.log(`power`, power);
-
-    setDisplayContent(`Power ${power ? "ON" : "OFF"}`);
+  const resetDisplay = useCallback(() => {
     const timeout = setTimeout(() => {
       setDisplayContent(<>&nbsp;</>);
     }, 1000);
     return () => {
       return clearTimeout(timeout);
     };
-  }, [power]);
+  }, []);
+
+  useEffect(() => {
+    // console.log(`power`, power);
+
+    setDisplayContent(`Power ${power ? "ON" : "OFF"}`);
+    return resetDisplay();
+  }, [power, resetDisplay]);
 
   useEffect(() => {
     // console.log(`bank`, bank);
 
     setDisplayContent(bank ? "Piano" : "Heater Kit");
-    const timeout = setTimeout(() => {
-      setDisplayContent(<>&nbsp;</>);
-    }, 1000);
-    return () => {
-      return clearTimeout(timeout);
-    };
-  }, [bank]);
+    return resetDisplay();
+  }, [bank, resetDisplay]);
 
   useEffect(() => {
     // console.log(`volume`, volume);
 
     setDisplayContent(`Volume ${volume}%`);
-    const timeout = setTimeout(() => {
-      setDisplayContent(<>&nbsp;</>);
-    }, 1000);
-    return () => {
-      return clearTimeout(timeout);
-    };
-  }, [volume]);
+    return resetDisplay();
+  }, [volume, resetDisplay]);
 
   return (
     <div className="Controls">
